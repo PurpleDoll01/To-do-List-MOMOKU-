749d07e322704be2ce182ffe28fb89d890dc23cd
Mariana Valencia <purple.mv@gmail.com>
2020-03-08 16:42:09 -0500

    Updates

---
+++ precache-manifest.966e5d60aae714529b9bdea7e691c163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6244ce35e346f52fb2d82ef314df3457",
+    "url": "/To-do-List-MOMOKU-/index.html"
+  },
+  {
+    "revision": "fd40c72e8e04b6508135",
+    "url": "/To-do-List-MOMOKU-/static/css/main.ad9c06e6.chunk.css"
+  },
+  {
+    "revision": "81733af27c8b424c9ff4",
+    "url": "/To-do-List-MOMOKU-/static/js/2.97f2f0c6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/To-do-List-MOMOKU-/static/js/2.97f2f0c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd40c72e8e04b6508135",
+    "url": "/To-do-List-MOMOKU-/static/js/main.68bc9585.chunk.js"
+  },
+  {
+    "revision": "3735b970797d955b8699",
+    "url": "/To-do-List-MOMOKU-/static/js/runtime-main.dd3ec972.js"
+  }
+]);
